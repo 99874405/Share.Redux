1a9a0a96c75199239385b596d92b6059a68bb270
free <99874405@qq.com>
2018-09-30 16:15:32 +0800

    x

--- src/components/f.jsx
+++ src/components/f.jsx
@@ -57,11 +57,13 @@ class Component extends React.Component {
     render() {
         return (
             <div>
-                <Progress type="circle" percent={this.props.count} />&nbsp;&nbsp;
-                <Button.Group>
-                    <Button icon="minus" onClick={this.decrement} />
-                    <Button icon="plus" onClick={this.increment} />
-                </Button.Group>
+                <Spin>
+                    <Progress type="circle" percent={this.props.count} />&nbsp;&nbsp;
+                    <Button.Group>
+                        <Button icon="minus" onClick={this.decrement} />
+                        <Button icon="plus" onClick={this.increment} />
+                    </Button.Group>
+                </Spin>
             </div>
         )
     }
