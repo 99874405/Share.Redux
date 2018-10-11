860c4701dbb731a2843fb7525cb46ca064ed4f8f
free <99874405@qq.com>
2018-10-11 21:55:44 +0800

    x

--- src/redux.js
+++ src/redux.js
@@ -1,2 +1,22 @@
 
 
+function createStore(reducer, middleware) {
+    let listener = () => {}
+    let state = reducer(undefined, {})
+    let store = {
+        getState() {
+            return state
+        },
+        dispatch(action) {
+            state = reducer(state, action); listener()
+        },
+        subscribe(listener) {
+            listener = listener
+        },
+    }
+}
+
+
+export default {
+    createStore
+}
