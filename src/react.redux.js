56fd522db4d67f8418f3cf831ed6602a1ab6fadc
free <99874405@qq.com>
2018-10-11 22:16:57 +0800

    x

--- src/react.redux.js
+++ src/react.redux.js
@@ -2,6 +2,17 @@ import React from 'react'
 import Types from 'prop-types'
 
 export class Provider extends React.Component {
+    
+    getChildContext() {
+        return {
+            store: this.props.store
+        }
+    }
+
+    static childContextTypes = {
+        store: Types.object
+    }
+
     render() {
         return this.props.children
     }
