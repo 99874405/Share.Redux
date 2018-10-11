90b9903f67609c54e277dc6f75a19708e73de5b0
free <99874405@qq.com>
2018-10-11 23:10:08 +0800

    x

--- src/redux.js
+++ src/redux.js
@@ -16,7 +16,7 @@ export function createStore(reducer, middlewares) {
     }
     
     if (Array.isArray(middlewares)) {
-        middlewares.forEach(middleware => store.dispatch = middleware(store)(store.dispatch))
+        middlewares.reverse().forEach(middleware => store.dispatch = middleware(store)(store.dispatch))
     }
 
     return store
