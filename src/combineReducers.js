6f5f234e2813adf90afcf075477173b7babcb821
guofengwu <guofengwu@wecash.net>
2019-04-08 20:49:00 +0800

    x

---
+++ src/combineReducers.js
@@ -0,0 +1,28 @@
+function combineReducers(reducer, middlewares) {
+    let cb = () => {}
+    let state = reducer(undefined, {})
+    let store = {
+        subscribe(callback) {
+            cb = callback
+        },
+        dispatch(action) {
+            cb(state = reducer(state, action))
+        },
+        getState() {
+            return state
+        },
+    }
+
+    if (Array.isArray(middlewares)) {
+        middlewares.reverse().forEach(middleware => {
+            store.dispatch = middleware()(store)(store.dispatch)
+        })
+    }
+
+    return store
+}
+
+
+export {
+    combineReducers
+}
