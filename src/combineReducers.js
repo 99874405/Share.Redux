c30007ddf7eb9f09bd368c3fede459366df378ac
guofengwu <guofengwu@wecash.net>
2019-04-08 21:15:00 +0800

    x

--- src/combineReducers.js
+++ src/combineReducers.js
@@ -1,5 +1,7 @@
-function combineReducers(reducers) {
-    
+function combineReducers(reducers, globalState = {}) {
+    return (state, action) => {
+        return Object.keys(reducers).forEach(modular => globalState[modular] = reducers[modular](state && state[modular], action)), globalState
+    }
 }
 
 
